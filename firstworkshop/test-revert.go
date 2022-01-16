68ae76be0790e10f8d0058d9f0a097f19848b4ec
watcharis <watcharis.s@fillgoods.co>
2022-01-16 18:32:17 +0700

    Revert "test revert 02"
    This reverts commit b77a078a9976d43fb6d456719e76602eb36f7ec1.

--- firstworkshop/test-revert.go
+++ firstworkshop/test-revert.go
@@ -1,64 +1,42 @@
 package firstworkshop
 
 import (
+	"fmt"
 	"sync"
 )
 
 func RevertTest() bool {
+	mock := 0
 	invalid := []string{}
 	valid := []string{}
 	o := "word"
 	mu := new(sync.Mutex)
 	isLimitExceed := false
-	ErrorStatusCodeLimitExceed := 1000
 	var err error
-	type Response struct {
-		Success    bool
-		StatusCode int
-	}
 
-	response := Response{}
-
-	if !response.Success && response.StatusCode == ErrorStatusCodeLimitExceed {
+	if mock > 2 {
+		fmt.Println("if isLimitExceed")
 		mu.Lock()
 		invalid = append(invalid, o)
+		// channelCheckIsLimitExceed <- true
 		isLimitExceed = true
 		mu.Unlock()
 	} else if err != nil {
+		fmt.Println("if invalid")
 		// log.Printf("[SubmitOrder] [ERROR] 'refID: %s' '[%+v]' create order through fillgoods failed %s\n", refID, req, err.Error())
 		mu.Lock()
 		invalid = append(invalid, o)
+		// channelCheckIsLimitExceed <- false
+		isLimitExceed = false
 		mu.Unlock()
 	} else {
+		fmt.Println("else valid")
 		mu.Lock()
 		valid = append(valid, o)
+		isLimitExceed = false
+		mock++
+		// channelCheckIsLimitExceed <- false
 		mu.Unlock()
 	}
 	return isLimitExceed
 }
-
-// if mock > 2 {
-// 	fmt.Println("if isLimitExceed")
-// 	mu.Lock()
-// 	invalid = append(invalid, o)
-// 	// channelCheckIsLimitExceed <- true
-// 	isLimitExceed = true
-// 	mu.Unlock()
-// } else if err != nil {
-// 	fmt.Println("if invalid")
-// 	// log.Printf("[SubmitOrder] [ERROR] 'refID: %s' '[%+v]' create order through fillgoods failed %s\n", refID, req, err.Error())
-// 	mu.Lock()
-// 	invalid = append(invalid, o)
-// 	// channelCheckIsLimitExceed <- false
-// 	isLimitExceed = false
-// 	mu.Unlock()
-// } else {
-// 	fmt.Println("else valid")
-// 	mu.Lock()
-// 	valid = append(valid, o)
-// 	isLimitExceed = false
-// 	mock++
-// 	// channelCheckIsLimitExceed <- false
-// 	mu.Unlock()
-// }
-// return isLimitExceed
